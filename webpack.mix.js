13d4dc4b9a240788f70e796781f4252c037a016c
Xolibas <andriy.xolibas@gmail.com>
2020-01-21 00:22:47 +0200

    Build

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix
+    .setPublicPath("public/build")
+    .setResourceRoot("build")
+    .js('resources/js/app.js', 'public/js')
+   .sass('resources/sass/app.scss', 'public/css')
+    .version();
